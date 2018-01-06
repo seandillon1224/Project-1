89860a9fa0a572ce844edaa96742c31aad16085b
Sean Dillon <seandillon1224@gmail.com>
2018-01-06 09:49:43 -0500

    new add

--- spotifypractice.js
+++ spotifypractice.js
@@ -71,7 +71,6 @@ $(".searchbutton").on("click", function spotifySongSearch (songTitle) {
   $(".car3").empty()
   $(".car4").empty()
   $(".car5").empty()
-  $('.artist-information').empty()
 
   $("#mainheader").text("Top 5 Most Relevant!")
   
@@ -101,13 +100,13 @@ $(".searchbutton").on("click", function spotifySongSearch (songTitle) {
         cover4=spotifySongResult.tracks.items[3].album.images[0].url
         cover5=spotifySongResult.tracks.items[4].album.images[0].url
 
-        replacedSong= $("#artist-information").val()
+        replacedSong= $(".artist-information").val()
         console.log(replacedSong)
 
         replacedSong = replacedSong.replace("spotify:track:2TpxZ7JUBn3uw46aR7qd6V", "spotifySongResult.tracks.items[0].uri")
         console.log(replacedSong)
 
-        $("#artist-information").append(replacedSong)
+        $(".artist-information").append(replacedSong)
 
 
 $(".carousel-item").empty();
